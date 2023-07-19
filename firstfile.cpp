d05a9d66b93d05c9288fb1f69e64084adb6c91e0
Yash pathak <assush698@gmail.com>
2023-07-19 10:18:26 +0530

    msg

--- firstfile.cpp
+++ firstfile.cpp
@@ -1,1 +1,45 @@
-avsfjehwkfg
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int s=0;
+        int e=nums.size();
+
+        int mid=(s+e)/2;
+
+        while(s<=e)
+        {
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+
+
+            if(nums[s]<=nums[mid])
+            {
+                if(nums[s]<=target && nums[mid]>= target)
+                {
+                    e=mid-1;
+                }
+                else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(target >= nums[mid]  && target <= nums[e])
+                {
+                    s=mid+1;                }
+
+                else{
+                    e=mid-1;
+                }
+
+            }
+
+            mid=(s+e)/2;
+        }
+
+        return -1;
+
+    }
+};
